6257a25f48a266a458cc5105021f5a2dc419205c
dameem4 <dameem_s@yahoo.com>
2020-09-10 16:47:53 +0100

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwH2jwYsRdyqHK9oopjmzb2yZKVAoC5KM",
+    authDomain: "clone-5298b.firebaseapp.com",
+    databaseURL: "https://clone-5298b.firebaseio.com",
+    projectId: "clone-5298b",
+    storageBucket: "clone-5298b.appspot.com",
+    messagingSenderId: "1047275320383",
+    appId: "1:1047275320383:web:50980e50592db42c24f48a",
+    measurementId: "G-SF1J8P6MYS"
+  };
